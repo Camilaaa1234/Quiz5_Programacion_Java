a41b0bdd9dc49307779a4e05c04b704a2676e6cf
Camilaaa1234 <141454456+Camilaaa1234@users.noreply.github.com>
2023-10-04 12:56:11 -0500

    Libro.java

---
+++ Libro.java
@@ -0,0 +1,22 @@
+public class Libro {
+    private String titulo;
+
+    private String autor ;
+    private int numeroPaginas ;
+
+    public Libro(String titulo, String autor, int numeroPaginas) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
+    }
+
+    public void setTitulo() {
+        this.titulo = titulo;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setNumeroPaginas(int numeroPaginas) {
+        this.numeroPaginas = numeroPaginas;
